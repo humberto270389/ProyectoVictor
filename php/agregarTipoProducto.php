e6e2550d41d9f7658f06cfd2e65e89c44978e632
humberto270389 <humberto_270389@hotmail.com>
2015-03-05 17:49:03 -0700

    nueva pag

---
+++ php/agregarTipoProducto.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+    <header>
+        <title></title>
+        <meta charset="utf-8">
+        <script type="text/javascript" src="../JS/jquery-1.11.2.min.js"></script>
+    </header>
+
+    <body>
+
+    </body>
+</html>
