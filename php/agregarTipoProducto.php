c0aa97412f5c80b9492708f7954bb289ebd1868f
humberto270389 <humberto_270389@hotmail.com>
2015-03-12 17:38:50 -0700

    cssTipo

--- php/agregarTipoProducto.php
+++ php/agregarTipoProducto.php
@@ -3,6 +3,7 @@
     <header>
         <title></title>
         <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="../css/adminTipoProducto.css">
         <script type="text/javascript" src="../JS/jquery-1.11.2.min.js"></script>
         <script>
             $(function(){
